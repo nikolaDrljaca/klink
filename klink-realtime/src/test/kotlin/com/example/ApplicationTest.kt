b8601ecd94cf56452e18d939f1c2b2061180fd82
nikolaDrljaca <devnikoladr@gmail.com>
2024-11-15 19:49:40 +0100

    ops: remove `realtime` tests

--- klink-realtime/src/test/kotlin/com/example/ApplicationTest.kt
+++ klink-realtime/src/test/kotlin/com/example/ApplicationTest.kt
@@ -1,21 +1,12 @@
 package com.example
 
-import io.ktor.client.request.*
-import io.ktor.client.statement.*
-import io.ktor.http.*
 import io.ktor.server.testing.*
 import kotlin.test.Test
-import kotlin.test.assertEquals
 
 class ApplicationTest {
     @Test
     fun testRoot() = testApplication {
         application {
-            klinkRoutes()
-        }
-        client.get("/health").apply {
-            assertEquals(HttpStatusCode.OK, status)
-            assertEquals("Service healthy.", bodyAsText())
         }
     }
 }
