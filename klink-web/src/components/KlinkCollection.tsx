2fe03ec70c74d4ece9cfec6202ea5ea1001d9719
nikolaDrljaca <devnikoladr@gmail.com>
2024-12-11 20:22:11 +0100

    KLINK-16: extract Klink list item component

--- klink-web/src/components/KlinkCollection.tsx
+++ klink-web/src/components/KlinkCollection.tsx
@@ -68,80 +68,30 @@ const KlinkCollection: Component = () => {
       </div>
 
       {/* Klink List - Container */}
-      <div class="container items-center w-full">
-        {/* List Item */}
-        <For each={state.klinks}>
-          {(item,) =>
-            <KlinkListItem
-              item={item}
-              pathKlinkId={pathKlinkId()}
-              onDeleteClick={() => onDeleteKlinkItemClick(item.id)}
-              onSelect={() => onSelectKlink(item.id)}
-              onCopyClick={() => onCopyKlink(item.id)}
-            />
-          }
-        </For>
-      </div>
+      <Show when={klinkNotEmpty()} fallback={<KlinkListEmpty />}>
+        <div class="container items-center w-full">
+          {/* List Item */}
+          <For each={state.klinks}>
+            {(item,) =>
+              <KlinkListItem
+                item={item}
+                pathKlinkId={pathKlinkId()}
+                onDeleteClick={() => onDeleteKlinkItemClick(item.id)}
+                onSelect={() => onSelectKlink(item.id)}
+                onCopyClick={() => onCopyKlink(item.id)}
+              />
+            }
+          </For>
+        </div>
+      </Show>
     </div>
   );
 }
 
-type KlinkListItemProps = {
-  item: Klink,
-  pathKlinkId?: string,
-  onDeleteClick: () => void,
-  onCopyClick: () => void,
-  onSelect: () => void
-}
-
-const KlinkListItem: Component<KlinkListItemProps> = (props) => {
-  const isSelected = () => props.pathKlinkId === props.item.id;
-  const classes = () => clsx(
-    'flex flex-col p-2 w-full justify-center border-b-2 border-zinc-900',
-    isSelected() && 'bg-neutral'
-  );
-
-  const deleteModal = createModal();
-  const shareModal = createModal();
-
-  const onEdit = () => {
-    //TODO: 
-    toast("Not implemented.");
-  }
-
+const KlinkListEmpty: Component = () => {
   return (
-    <div class={classes()}>
-      {/* Top Anchor */}
-      <a class="flex flex-col w-full px-4 hover:cursor-pointer" onClick={props.onSelect}>
-        <p class="text-xs font-light text-zinc-400">Updated at 15:43</p>
-        <p class="text-lg">{props.item.name}</p>
-        <span class="text-sm py-2">{props.item.description}</span>
-      </a>
-      {/* Button Row */}
-      <div class="flex flex-row items-center justify-around w-full pt-4 pl-4">
-        <button class="btn btn-circle btn-ghost btn-sm" onClick={onEdit}>
-          <Edit size={14} />
-        </button>
-        <button class="btn btn-circle btn-ghost btn-sm" onClick={props.onCopyClick}>
-          <Copy size={14} />
-        </button>
-        <button class="btn btn-circle btn-ghost btn-sm" onClick={shareModal.controller.open}>
-          <Share size={14} />
-        </button>
-        <button class="btn btn-circle btn-sm btn-ghost" onClick={() => deleteModal.controller.open()}>
-          <Trash size={14} />
-        </button>
-
-        <deleteModal.Modal>
-          <DeleteKlinkModal
-            klink={props.item}
-            onClose={deleteModal.controller.close}
-            onSubmit={props.onDeleteClick} />
-        </deleteModal.Modal>
-        <shareModal.Modal>
-          <ShareKlinkModal item={props.item} />
-        </shareModal.Modal>
-      </div>
+    <div class="flex flex-col w-full h-full items-center justify-center">
+      <p>Use the 'Create' button to make a new Klink.</p>
     </div>
   );
 }
