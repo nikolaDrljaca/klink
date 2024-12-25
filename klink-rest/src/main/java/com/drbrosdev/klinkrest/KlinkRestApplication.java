c0fb13f4075f7bec4068bb644330734ae32c17ab
nikolaDrljaca <devnikoladr@gmail.com>
2024-12-25 14:40:53 +0100

    hotfix: adjust `rest` security configuration

--- klink-rest/src/main/java/com/drbrosdev/klinkrest/KlinkRestApplication.java
+++ klink-rest/src/main/java/com/drbrosdev/klinkrest/KlinkRestApplication.java
@@ -2,9 +2,6 @@ package com.drbrosdev.klinkrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class KlinkRestApplication {
@@ -13,15 +10,4 @@ public class KlinkRestApplication {
         SpringApplication.run(KlinkRestApplication.class, args);
     }
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
-            }
-        };
-    }
 }
