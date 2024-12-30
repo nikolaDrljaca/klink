9c209ab6cd1e6e2a13145b51e4b484a97b933c9e
Nemanja <necobn@hotmail.com>
2024-12-30 17:18:04 +0100

    [KLINK-32a] Add timestamp for klinkEntry model

--- klink-rest/src/main/java/com/drbrosdev/klinkrest/domain/mapper/KlinkDomainServiceMapper.java
+++ klink-rest/src/main/java/com/drbrosdev/klinkrest/domain/mapper/KlinkDomainServiceMapper.java
@@ -7,6 +7,7 @@ import com.drbrosdev.klinkrest.persistence.entity.KlinkKeyEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Mapper(componentModel = "spring")
@@ -15,10 +16,18 @@ public interface KlinkDomainServiceMapper {
     @Mapping(target = "readKey", source = "key.readKey")
     @Mapping(target = "writeKey", source = "key.writeKey")
     @Mapping(target = "id", source = "entity.id")
-    @Mapping(target = "updatedAt", source = "entity.modifiedAt")
+    @Mapping(target = "updatedAt", expression = "java(mapLatestCreatedAt(entries))")
     KlinkDto mapTo(
             final KlinkEntity entity,
             final List<KlinkEntryEntity> entries,
             final KlinkKeyEntity key);
 
+    default LocalDateTime mapLatestCreatedAt(
+            List<KlinkEntryEntity> entries) {
+        return entries.stream()
+                .map(KlinkEntryEntity::getCreatedAt)
+                .max(LocalDateTime::compareTo)
+                .orElse(null);
+    }
 }
+
