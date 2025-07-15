3a114cfef18a1f3cd5685b2112effdb876e4c40e
nikolaDrljaca <devnikoladr@gmail.com>
2025-07-15 19:59:02 +0200

    feature: remove realtime service

--- klink-realtime/README.md
+++
@@ -1,1 +0,0 @@
-# Klink Realtime Service
--- klink-realtime/src/main/kotlin/com/example/util/StringExt.kt
+++
@@ -1,9 +0,0 @@
-package com.example.util
-
-import io.ktor.http.*
-import java.util.*
-
-fun String.isUuid(): Boolean =
-    kotlin.runCatching { UUID.fromString(this) }.isSuccess
-
-fun String.isUrl(): Boolean = parseUrl(this) != null
