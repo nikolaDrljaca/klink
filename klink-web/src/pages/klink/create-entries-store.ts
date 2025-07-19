5bdb4b56c8b8a14f78c2e541a686280302e5ca4e
nikolaDrljaca <devnikoladr@gmail.com>
2025-07-19 19:29:17 +0200

    feature: accept paste from anywhere for open klink

---
+++ klink-web/src/pages/klink/create-entries-store.ts
@@ -0,0 +1,64 @@
+import { useNavigate } from "@solidjs/router";
+import { createEffect } from "solid-js";
+import toast from "solid-toast";
+import createPasteKeyEvent from "~/lib/create-paste-key-event";
+import { readLastClipboardEntry } from "~/lib/read-clipboard";
+import useKlinkEntries from "~/stores/klink-entry-store";
+import { isUrl } from "~/types/domain";
+
+export default function createEntriesStore() {
+  const store = useKlinkEntries();
+  const navigate = useNavigate();
+
+  // setup paste keyboard handling
+  const pasteKey = createPasteKeyEvent();
+  createEffect(async () => {
+    if (!pasteKey()) {
+      return;
+    }
+    try {
+      const entry = await readLastClipboardEntry();
+      if (isUrl(entry)) {
+        await store().addEntry(entry);
+      }
+    } catch (e) {
+      toast.error("Failed to read clipboard!");
+    }
+  });
+
+  const handlePaste = async (e: ClipboardEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const pastedValue = e.clipboardData.getData("text/plain");
+    await store().addEntry(pastedValue);
+  };
+
+  const handleEnter = async (event: Event, value: string) => {
+    event.preventDefault();
+    event.stopPropagation();
+    await store().addEntry(value);
+  };
+
+  const handleDelete = async (value: string) => {
+    await store().removeEntry(value);
+  };
+
+  const handleBack = () => {
+    if (history.length <= 2) {
+      navigate("/c", {
+        replace: true,
+      });
+    } else {
+      navigate(-1);
+    }
+  };
+
+  return () => ({
+    entries: store().entries,
+    klink: store().klink,
+    handlePaste,
+    handleEnter,
+    handleDelete,
+    handleBack,
+  });
+}
