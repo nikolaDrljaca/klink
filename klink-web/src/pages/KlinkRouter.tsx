550bfd11259c2e4f5c42acc5ef61c845747b43f9
nikolaDrljaca <devnikoladr@gmail.com>
2025-07-22 20:44:53 +0200

    fix: implement nested routing

--- klink-web/src/pages/KlinkRouter.tsx
+++ klink-web/src/pages/KlinkRouter.tsx
@@ -1,5 +1,5 @@
 import { MatchFilters, Navigate, Route, Router } from "@solidjs/router";
-import { Component } from "solid-js";
+import { Component, ParentComponent } from "solid-js";
 import RootLayout from "~/components/layout/RootLayout";
 import KlinkCollectionRoute from "~/pages/collection/KlinkCollectionRoute";
 import KlinkImportRoute from "~/pages/import/KlinkImportRoute";
@@ -7,6 +7,7 @@ import KlinkRoute from "~/pages/klink/KlinkRoute";
 import ComingSoonRoute from "~/pages/status/ComingSoonRoute";
 import NotFoundRoute from "~/pages/status/404Route";
 import KlinkSettingsRoute from "./settings/KlinkSettingsRoute";
+import KlinkSidebar from "~/components/KlinkSidebar";
 
 const uuidRouteFilter: MatchFilters = {
   klinkId: (v: string) => v.length === 36,
@@ -16,25 +17,44 @@ const RedirectToRoot: Component = () => {
   return <Navigate href="/c" />;
 };
 
+const SidebarWrapper: ParentComponent = (props) => {
+  return (
+    <div class="flex flex-col lg:flex-row h-screen">
+      <div class="w-full lg:w-1/6 lg:h-full border-base-300 lg:border-r-2">
+        <KlinkSidebar />
+      </div>
+      {props.children}
+    </div>
+  );
+};
+
 const KlinkRouter: Component = () => {
   return (
     <Router root={RootLayout}>
       <Route path="/" component={RedirectToRoot} />
+
       <Route
         path="/c"
-        component={KlinkCollectionRoute}
-      />
-      <Route
-        path="/c/:klinkId/i"
-        component={KlinkImportRoute}
-        matchFilters={uuidRouteFilter}
-      />
-      <Route
-        path="/c/:klinkId"
-        component={KlinkRoute}
-        matchFilters={uuidRouteFilter}
-      />
-      <Route path="/settings" component={KlinkSettingsRoute} />
+        component={SidebarWrapper}
+      >
+        {/* Klink Collection nested routes */}
+        <Route path="/" component={KlinkCollectionRoute}>
+          <Route path="/" component={() => <></>} />
+          <Route
+            path="/:klinkId"
+            component={KlinkRoute}
+            matchFilters={uuidRouteFilter}
+          />
+          <Route
+            path="/:klinkId/i"
+            component={KlinkImportRoute}
+            matchFilters={uuidRouteFilter}
+          />
+        </Route>
+
+        <Route path="/settings" component={KlinkSettingsRoute} />
+      </Route>
+
       <Route path="/about" component={ComingSoonRoute} />
       <Route path="*param" component={NotFoundRoute} />
     </Router>
--- klink-web/src/pages/klink/KlinkRoute.tsx
+++ klink-web/src/pages/klink/KlinkRoute.tsx
@@ -1,25 +1,12 @@
 import { Component, Show } from "solid-js";
-import KlinkSidebar from "~/components/KlinkSidebar";
-import KlinkCollection from "~/pages/collection/components/KlinkCollection";
 import KlinkEntries from "~/pages/klink/components/KlinkEntries";
 import KlinkNotFound from "./components/KlinkNotFound";
 import { useSelectedKlink } from "~/stores/klink-hooks";
 
 const KlinkRoute: Component = () => {
   const klink = useSelectedKlink();
-
   return (
-    <div class="flex flex-col lg:flex-row h-screen">
-      {/* Sidebar */}
-      <div class="w-full lg:w-1/6 lg:h-full border-base-300 lg:border-r-2">
-        <KlinkSidebar />
-      </div>
-
-      {/* KlinkCollection */}
-      <div class="hidden lg:block lg:w-2/6 h-full border-base-300 lg:border-r-2">
-        <KlinkCollection />
-      </div>
-
+    <>
       {/* Klink Details */}
       <div class="w-full lg:w-3/6 xl:w-2/6 h-full border-base-300 lg:border-r-2">
         {/* TODO: Modify fallback */}
@@ -41,7 +28,7 @@ const KlinkRoute: Component = () => {
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
