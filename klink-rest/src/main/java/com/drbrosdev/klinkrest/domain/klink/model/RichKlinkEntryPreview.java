bc8bae994f84b7fba7d1c7ae0a685648706a8a64
nikolaDrljaca <devnikoladr@gmail.com>
2025-07-27 20:39:23 +0200

    KLINK-62: scaffold implementation for jsoup document parser

--- klink-rest/src/main/java/com/drbrosdev/klinkrest/domain/klink/model/RichKlinkEntryPreview.java
+++ klink-rest/src/main/java/com/drbrosdev/klinkrest/domain/klink/model/RichKlinkEntryPreview.java
@@ -1,17 +1,18 @@
 package com.drbrosdev.klinkrest.domain.klink.model;
 
+import jakarta.annotation.Nullable;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Value;
 
-import java.util.UUID;
-
 @Value
 @Builder
 @AllArgsConstructor(access = AccessLevel.PRIVATE) //Hides the constructor to force usage of the Builder.
-public class EnrichLinkJob {
-    UUID klinkEntryId;
+public class RichKlinkEntryPreview {
+    @Nullable
+    String title;
 
-    String value;
+    @Nullable
+    String description;
 }
--- klink-rest/src/main/java/com/drbrosdev/klinkrest/domain/klink/usecase/EnrichLink.java
+++ klink-rest/src/main/java/com/drbrosdev/klinkrest/domain/klink/usecase/EnrichLink.java
@@ -1,21 +1,40 @@
 package com.drbrosdev.klinkrest.domain.klink.usecase;
 
 import com.drbrosdev.klinkrest.domain.klink.model.EnrichLinkJob;
+import com.drbrosdev.klinkrest.domain.klink.model.KlinkEntry;
 import com.drbrosdev.klinkrest.utils.UseCase;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @UseCase
 @Log4j2
+@RequiredArgsConstructor
 public class EnrichLink {
     // should handle exceptions
     // call the http client
     // pass result to parser
     // pass parser result to db
     // notify triggers
+
+    private final GenerateUrlPreview generateUrlPreview;
+
     public void execute(EnrichLinkJob job) {
         try {
             // TODO impl
-            Thread.sleep(3_000);
+            // parse rich link data
+            var preview = generateUrlPreview.execute(KlinkEntry.builder()
+                            .value(job.getValue())
+                            .build()) // TODO this stinks a bit
+                    .orElse(null);
+            if (preview == null) {
+                log.warn("Unable to generate rich link preview for {}", job.getValue());
+                return;
+            }
+            //TODO pass preview to repo for storage
+            // TODO Remove log
+            log.info("Generated link preview: {}", preview);
+            // TODO Remove sleep
+            Thread.sleep(2_000);
         } catch (Exception e) {
             log.error("Failed to enrich {}.", job.getValue());
         }
