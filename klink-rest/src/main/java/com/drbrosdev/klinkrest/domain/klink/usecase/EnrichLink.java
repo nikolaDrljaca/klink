b1082301b817a10aa17fcba51d44240620d016b3
nikolaDrljaca <devnikoladr@gmail.com>
2025-07-29 20:22:08 +0200

    KLINK-61: implement explicit notification when enrich finishes

--- klink-rest/src/main/java/com/drbrosdev/klinkrest/domain/klink/usecase/EnrichLink.java
+++ klink-rest/src/main/java/com/drbrosdev/klinkrest/domain/klink/usecase/EnrichLink.java
@@ -2,10 +2,15 @@ package com.drbrosdev.klinkrest.domain.klink.usecase;
 
 import com.drbrosdev.klinkrest.domain.klink.model.EnrichLinkJob;
 import com.drbrosdev.klinkrest.domain.klink.model.KlinkEntry;
+import com.drbrosdev.klinkrest.domain.klink.model.KlinkEntryChangeEvent;
+import com.drbrosdev.klinkrest.domain.klink.model.Operation;
 import com.drbrosdev.klinkrest.domain.klink.model.RichKlinkEntryPreview;
 import com.drbrosdev.klinkrest.persistence.entity.KlinkRichEntryEntity;
 import com.drbrosdev.klinkrest.persistence.repository.KlinkRichEntryRepository;
 import com.drbrosdev.klinkrest.utils.UseCase;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.transaction.annotation.Propagation;
@@ -20,19 +25,16 @@ import static java.util.UUID.randomUUID;
 @Log4j2
 @RequiredArgsConstructor
 public class EnrichLink {
-    // should handle exceptions
-    // call the http client
-    // pass result to parser
-    // pass parser result to db
-    // notify triggers
 
     private final GenerateUrlPreview generateUrlPreview;
 
     private final KlinkRichEntryRepository richEntryRepository;
 
+    private final ObjectMapper objectMapper = new ObjectMapper()
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
     public void execute(EnrichLinkJob job) {
         try {
-            // TODO impl
             // parse rich link data
             var preview = generateUrlPreview.execute(entry(job))
                     .orElse(null);
@@ -44,11 +46,8 @@ public class EnrichLink {
             createRichEntry(
                     job.getKlinkEntryId(),
                     preview);
-            // TODO trigger pg_notify for klink entries - jdbcTemplate call
-            // TODO Remove log
-            log.info("Generated link preview: {}", preview);
-            // TODO Remove sleep
-            Thread.sleep(2_000);
+            // trigger notify change for entries
+            notifyEntryChange(job);
         } catch (Exception e) {
             log.error("Failed to enrich {}.", job.getValue());
         }
@@ -66,6 +65,24 @@ public class EnrichLink {
                 .createdAt(LocalDateTime.now())
                 .build();
         richEntryRepository.save(entry);
+    }
+
+    protected void notifyEntryChange(EnrichLinkJob job) {
+        var event = KlinkEntryChangeEvent.builder()
+                .operation(Operation.UPDATED)
+                .row(KlinkEntryChangeEvent.Row.builder()
+                        .klinkId(job.getKlinkId().toString())
+                        .value("")
+                        .id("")
+                        .build())
+                .build();
+        try {
+            var rawEvent = objectMapper.writeValueAsString(event);
+            richEntryRepository.notifyEntryChanged(rawEvent);
+        } catch (JsonProcessingException e) {
+            // should never happen
+            log.error("Failed to serialize KlinkEntryChangeEvet -- SHOULD NEVER HAPPEN!");
+        }
     }
 
     private static KlinkEntry entry(EnrichLinkJob job) {
