7ac22f8081cdcec731867b346a2f4535cc7203a5
nikolaDrljaca <devnikoladr@gmail.com>
2025-07-31 15:39:33 +0200

    fix: session stream and user agent for jsoup

--- klink-rest/src/main/java/com/drbrosdev/klinkrest/framework/JsoupGenerateUrlPreview.java
+++ klink-rest/src/main/java/com/drbrosdev/klinkrest/framework/JsoupGenerateUrlPreview.java
@@ -6,6 +6,7 @@ import com.drbrosdev.klinkrest.domain.klink.usecase.GenerateUrlPreview;
 import com.drbrosdev.klinkrest.utils.UseCase;
 import jakarta.annotation.Nullable;
 import lombok.extern.log4j.Log4j2;
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
@@ -19,6 +20,12 @@ import static org.apache.commons.lang3.StringUtils.isNotBlank;
 @Log4j2
 public class JsoupGenerateUrlPreview implements GenerateUrlPreview {
 
+    private final Connection session = Jsoup.newSession()
+            //OR try facebookcatalog/1.0
+            .userAgent("facebookexternalhit/1.1")
+            // 6 seconds
+            .timeout(6000);
+
     @Override
     public Optional<RichKlinkEntryPreview> execute(KlinkEntry entry) {
         try {
@@ -26,8 +33,15 @@ public class JsoupGenerateUrlPreview implements GenerateUrlPreview {
             if (!validUrl(entry)) {
                 return Optional.empty();
             }
+            // TODO Refactor to use the StreamingReader
+            session.newRequest(entry.getValue())
+                    .method(Connection.Method.GET)
+                    .execute()
+                    .streamParser()
+                    .close();
+            // TODO Investigate above refactor
             // load document from URL
-            var document = Jsoup.connect(entry.getValue())
+            var document = session.newRequest(entry.getValue())
                     .get();
             return Optional.of(RichKlinkEntryPreview.builder()
                     .title(parseTitle(document))
